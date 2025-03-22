0bb1971f5b1d1744c77b0748a10b1f68247e7a97
Zhasmin <jas.hovhannisyan@gmail.com>
2025-03-22 17:58:58 +0400

    Initial commit: Bubble sort

--- Semester 1/bubbleSort.py
+++ Semester 1/bubbleSort.py
@@ -1,1 +1,12 @@
-print("aaa")
+def bubble_sort(arr):
+    length = len(arr)
+    for i in range(length):
+        for j in range(length - i - 1):
+            if arr[j] > arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+    return arr
+
+some_array = [6,4,29,7,3,1,40,2,-1]
+result = bubble_sort(some_array)
+print(result)
+            
