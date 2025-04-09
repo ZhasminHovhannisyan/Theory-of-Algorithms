914c8bb6b47fbb6fbf067d8955c830ec8d949bfd
Zhasmin <jas.hovhannisyan@gmail.com>
2025-04-10 02:53:40 +0400

    add optimized function

--- Semester 2/BFS.py
+++ Semester 2/BFS.py
@@ -1,35 +1,3 @@
-n = 13
-graph = {0: [7, 9, 11],
-         1: [],
-         2: [],
-         3: [2, 4],
-         4: [],
-         5: [],
-         6: [5],
-         7: [6, 3, 11],
-         8: [1, 12],
-         9: [8, 10, 0],
-         10: [1],
-         11: [], 
-         12: [2]
-         }
-
-first_node = int(input("enter the first node: "))
-
-
-
-############ version 1 ############
-
-# queue = [first_node]
-
-# for k in queue:
-#     for v in graph[k]:
-#         if v not in queue:
-#             queue.append(v)
-
-# print(queue)
-
-############### version 2 ################
 def bfs(graph, start):
     visited = set()  
     queue = [start]  
@@ -45,11 +13,45 @@ def bfs(graph, start):
             queue.extend(neighbour for neighbour in graph[node] if neighbour not in visited)
 
 
+# time complexity of queue.pop(0) is O(n)
+
+# algorithm optimization: use popleft(), time complexity is O(1)
+
+from collections import deque
+
+def bfs_optim(graph, start):
+    visited = set()
+    queue = deque([start])
+
+    while queue:
+        node = queue.popleft()  # O(1)
+        if node not in visited:
+            print(node, end=" ")
+            visited.add(node)
+            queue.extend(neighbour for neighbour in graph[node] if neighbour not in visited)
+
+
+n = 13
+graph = {0: [7, 9, 11],
+         1: [],
+         2: [12],
+         3: [2, 4],
+         4: [],
+         5: [],
+         6: [5],
+         7: [6, 3, 11],
+         8: [1, 12],
+         9: [8, 10, 0],
+         10: [1],
+         11: [], 
+         12: [2]
+}
+
+first_node = int(input("enter the first node: "))
+
 print("BFS starting with your input node:")
+print("BFS result: ")
 bfs(graph, first_node)
 
-
-############### homework: BFS with pseudocode from slides ##############
-
-
-
+print("/nBFS optimized: ")
+bfs_optim(graph, first_node)
