ffea28589f4de4c78c7311ddc2f5af90fec188d4
Zhasmin <jas.hovhannisyan@gmail.com>
2025-04-10 16:11:14 +0400

    graph visualisazion updated for connected components

--- Semester 2/graph_visualisation.py
+++ Semester 2/graph_visualisation.py
@@ -1,37 +1,80 @@
 import matplotlib.pyplot as plt
 import random
 
-def create_graph():
-    edges = input("Enter the connections in this format A-B, B-C, etc: ").split(',')
-    node_pairs = [] 
-    nodes = set()
-    
-    for edge in edges:
-        pairs = edge.strip().split('-')
-        node_pairs.append(pairs)
-        nodes.update(pairs)
-    
-    return node_pairs, list(nodes)
+# -------- DFS and Connected Components ----------
+def dfs(graph, at, visited, result):
+    if visited[at]:
+        return
+    visited[at] = True
+    result.append(at)
+    for neighbor in graph[at]:
+        if not visited[neighbor]:
+            dfs(graph, neighbor, visited, result)
+    return result
 
-def draw_graph(graph, nodes):
-    plt.figure(figsize=(5, 4))
+def conncomp(graph):
+    visited = {node: False for node in graph}
+    components = []
+
+    for node in graph:
+        if not visited[node]:
+            component = []
+            dfs(graph, node, visited, component)
+            components.append(component)
+
+    return components
+
+# -------- Graph Visualization ----------
+def draw_graph_with_components(graph, components):
+    plt.figure(figsize=(8, 6))
+    nodes = list(graph.keys())
     
-    # Generate random positions for nodes
     positions = {node: (random.uniform(0, 10), random.uniform(0, 10)) for node in nodes}
     
-    for edge in graph:
-        x_values = [positions[edge[0]][0], positions[edge[1]][0]]
-        y_values = [positions[edge[0]][1], positions[edge[1]][1]]
-        plt.plot(x_values, y_values, 'gray')
-    
-    # Draw nodes
+    colors = plt.cm.tab20.colors
+    component_colors = {}
+    for i, component in enumerate(components):
+        for node in component:
+            component_colors[node] = colors[i % len(colors)]
+
+    # Draw edges
+    for node, neighbors in graph.items():
+        for neighbor in neighbors:
+            x_vals = [positions[node][0], positions[neighbor][0]]
+            y_vals = [positions[node][1], positions[neighbor][1]]
+            plt.plot(x_vals, y_vals, color='gray', zorder=1)
+
+    # Draw nodes with component colors
     for node, (x, y) in positions.items():
-        plt.scatter(x, y, color='lightblue', s=700)
-        plt.text(x, y, node, fontsize=10, ha='center', va='center')
-    
-    plt.axis('off') 
-    plt.show()  
+        plt.scatter(x, y, color=component_colors[node], s=700, zorder=2)
+        plt.text(x, y, str(node), fontsize=10, ha='center', va='center', zorder=3)
 
-graph, nodes = create_graph()  
-draw_graph(graph, nodes)
+    plt.axis('off')
+    plt.title('Connected Components Visualization')
+    plt.show()
 
+# -------- Main Graph Data --------
+graph = {
+    0: [4, 8, 13, 14],
+    1: [5],
+    2: [9, 15],
+    3: [9],
+    4: [0, 8],
+    5: [1, 16, 17],
+    6: [7, 11],
+    7: [6],
+    8: [0, 4, 14],
+    9: [2, 3, 15],
+    10: [15],
+    11: [6],
+    12: [],
+    13: [0, 14],
+    14: [0, 13, 8],
+    15: [2, 9, 10],
+    16: [5],
+    17: [5]
+}
+
+components_list = conncomp(graph)
+print("Connected Components:", components_list)
+draw_graph_with_components(graph, components_list)
